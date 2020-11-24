0938efe06f132285d8d4c997ee3b81b0a42f587b
Rocco <roccodevs@protonmail.com>
2020-11-24 21:47:32 +0100

    Added all files

---
+++ RoccoServe.Framework.Server/ServerBuilder.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RoccoServe.Framework.Server
+{
+    public class ServerBuilder
+    {
+
+        public ServerBuilder(IServiceProvider serviceProvider)
+        {
+            ApplicationServices = serviceProvider;
+        }
+
+        public IList<ServerBinding> Bindings { get; } = new List<ServerBinding>();
+
+        public TimeSpan ShutdownTimeout { get; set; } = TimeSpan.FromSeconds(5);
+
+        public TimeSpan HeartBeatInterval { get; set; } = TimeSpan.FromSeconds(1);
+
+        public IServiceProvider ApplicationServices { get; }
+
+        public Server Build()
+        {
+            return new Server(this);
+        }
+    }
+}
---
+++ RoccoServe.Framework.Server/ServiceProviderExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+
+namespace RoccoServe.Framework.Server
+{
+    internal static class ServiceProviderExtensions
+    {
+        internal static ILoggerFactory GetLoggerFactory(this IServiceProvider serviceProvider)
+        {
+            return (ILoggerFactory)serviceProvider?.GetService(typeof(ILoggerFactory)) ?? NullLoggerFactory.Instance;
+        }
+    }
+}
