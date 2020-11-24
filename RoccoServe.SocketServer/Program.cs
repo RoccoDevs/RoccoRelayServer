3c41027a2dc48998b7c5f7df7681c9a7c105d387
Tom Esendam <tom.esendam@gmail.com>
2020-11-24 21:47:32 +0100

    Added all files

---
+++ RoccoServe.SocketServer/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using RoccoServe.Framework.Server;
+
+namespace RoccoServe.SocketServer
+{
+    class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var services = new ServiceCollection();
+            services.AddLogging(builder =>
+            {
+                builder.SetMinimumLevel(LogLevel.Debug);
+                builder.AddConsole();
+            });
+
+            var serviceProvider = services.BuildServiceProvider();
+
+            var server = new ServerBuilder(serviceProvider)
+                ;
+        }
+    }
+}
